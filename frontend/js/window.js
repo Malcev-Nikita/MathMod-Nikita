796a0a1990a7c9d4c9ca6b2f357094ea8145cab8
Никита <mnd1work@gmail.com>
2022-11-21 15:43:52 +0400

    commit 21.11.22 (2)

--- frontend/js/window.js
+++ frontend/js/window.js
@@ -3,18 +3,26 @@ let main__button = document.getElementsByClassName("main__button")
 
 let closebtn = document.getElementsByClassName("example__close")
 
+let j;
+
 
 for (let i = 0; i < main__button.length; i++)
 {
-    main__button[i].addEventListener("click", function() {
+    main__button[i].addEventListener("click", function() 
+    {
+        if (i == 0) j = 1
+        else if (i == 1) j = 0
 
-        if (main__button[i].classList.contains("example_active"))
+        if (example[j].classList.contains("example_active"))
         {
-            main__button[i].classList.remove("example_active")
-            main__button[i].classList.add("example_unactive")
+            example[j].classList.remove("example_active")
+            example[j].classList.add("example_unactive")
+
+            example[i].classList.add("example_active")
+            example[i].classList.remove("example_unactive")
         }
 
-        else if (!main__button[i].classList.contains("example_active"))
+        else
         {
             example[i].classList.add("example_active")
             example[i].classList.remove("example_unactive")
@@ -26,14 +34,9 @@ for (let i = 0; i < main__button.length; i++)
 
 for (let i = 0; i < closebtn.length; i++)
 {
-    closebtn[i].addEventListener("click", function(){
-
-        if (!example[i].classList.contains("example_active"))
-        {
-            example[j].classList.add(example[j].classList[0] + "_unactive")
-        }
-
-        example[j].classList.remove(example[j].classList[0] + "_active")
-    
+    closebtn[i].addEventListener("click", function()
+    {
+        example[i].classList.remove("example_active")
+        example[i].classList.add("example_unactive")
     })
 }
